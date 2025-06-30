9d24d80cdf480e2bfb511a8333615d1e221eb660
Polinaa123 <polina.antonoff@gmail.com>
2025-06-30 16:41:27 +0300

    Add new assets and update project structure with index.html and robots.txt

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  root: "puusti-waitlist",
+  root: ".",
   server: {
     host: "::",
     port: 8080,
@@ -22,8 +22,8 @@ export default defineConfig(({ mode }) => ({
   },
 
   build:{
-    outDir: path.resolve(__dirname, "dist"),
-    emptyOutDir: true,
+    outDir: "dist",
     assetsDir: 'assets',
+    emptyOutDir: true,
   }
 }));
